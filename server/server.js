6a85ea87db70334e52041b8b05a2bedf3f91856c
Antonic111 <antoniodeluca111@hotmail.com>
2025-08-17 00:06:48 -0400

    Fix CORS to handle credentials properly with specific origins

--- server/server.js
+++ server/server.js
@@ -21,7 +21,12 @@ app.use((req, res, next) => {
   console.log('🔥 URL:', req.url);
   console.log('🔥 Origin:', req.headers.origin);
   
-  res.header('Access-Control-Allow-Origin', '*');
+  // Handle credentials properly - can't use wildcard with credentials
+  const origin = req.headers.origin;
+  if (origin) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
