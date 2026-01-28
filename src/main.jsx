1ef03398d70ccafe80a7f43508eaab70dfac9296
Antonic111 <antoniodeluca111@hotmail.com>
2026-01-28 06:29:13 -0500

    Speed insights added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 const enableAnalytics = import.meta.env.VITE_ENABLE_VERCEL_ANALYTICS === 'true';
 
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     {enableAnalytics ? <Analytics debug={false} /> : null}
+    <SpeedInsights />
   </StrictMode>,
 )
